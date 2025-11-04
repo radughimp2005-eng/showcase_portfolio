b652c4348aaba502b2efcfcdb9b80e44a6984175
radughimp2005-eng <radughimp2005@gmail.com>
2025-11-05 00:39:58 +0100

    added the home page for picoo project page

--- app/picoo/page.tsx
+++ app/picoo/page.tsx
@@ -1,5 +1,11 @@
 export default function Picoo() {
   return (
-    <h1 className="hello">Hello world</h1>
+    <section className="picoo-home">
+      <div className="picoologotext">
+        <img src="/images/picooimg.png" className = "picooimg"alt="Picoo image" />
+        <p>A simple, user friendly website that helps primary schools teachers create a quiz fast and efortless.</p>
+      </div>
+      <img src="/images/picoo.gif"  className="picoogif" alt="Picoo gif" />
+    </section>
   );
 }
