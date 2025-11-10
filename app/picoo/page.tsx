b284a860b694bada07490685ded8fc0976c949f2
radughimp2005-eng <radughimp2005@gmail.com>
2025-11-10 18:12:15 +0100

    small change

--- app/picoo/page.tsx
+++ app/picoo/page.tsx
@@ -3,7 +3,7 @@ export default function Picoo() {
     <main>
       <section className="picoo-home">
         <div className="picoologotext">
-          <img src="/images/picooimg.png" className = "picooimg"alt="Picoo image" />
+          <img src="/images/picooimg.png" className = "picoo1logo"alt="Picoo image" />
           <p>A simple, user friendly website that helps primary schools teachers create a quiz fast and efortless.</p>
         </div>
         <img src="/images/picoo.gif"  className="picoogif" alt="Picoo gif" />
